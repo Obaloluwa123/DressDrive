504dbd7dee3bbb188e95d34cc9a428efcdf4523b
Arnold Bhebhe <legendaryarnoldb@gmail.com>
2023-02-12 00:51:43 -0600

    update

--- dressdrivep/weatherdress/models.py
+++ dressdrivep/weatherdress/models.py
@@ -2,6 +2,26 @@ from statistics import mode
 from django.db import models
 
 
-# Create your models here.
-# class Profile(models.Model):
-    
+class ActivityForm(models.Model):
+    MOOD_CHOICES = [
+        ("happy", "Happy"),
+        ("sad", "Sad"),
+        ("excited", "Excited"),
+        ("relaxed", "Relaxed"),
+        ("energized", "Energized"),
+    ]
+    mood = models.CharField(max_length=10, choices=MOOD_CHOICES)
+    temperature = models.FloatField()
+
+    def __str__(self):
+        return self.mood
+
+
+class Activity(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    mood = models.CharField(max_length=100)
+    temperature = models.FloatField()
+
+    def __str__(self):
+        return self.name
--- dressdrivep/weatherdress/views.py
+++ dressdrivep/weatherdress/views.py
@@ -9,59 +9,53 @@ import requests
 import googlemaps
 from geopy.geocoders import Nominatim
 from .forms import *
-# from geopy.geocoders import Nominatim
 from django.contrib.auth.models import User
-# import json
-import json
-
-
-
-
+import numpy as np
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer
+from django.shortcuts import render
+from .models import ActivityForm
 
 
 def index(request):
     context = {}
     if request.user.is_authenticated:
-        return redirect('home')
+        return redirect("home")
 
     if request.method == "POST":
         form = UserRegistrationForm(request.POST)
-      
+
         if form.is_valid():
-            email = form.cleaned_data['email']
+            email = form.cleaned_data["email"]
             try:
                 User.objects.get(email=email)
                 error = "E-mail already registered!"
-                context['error'] = error
-                context['form'] = form
-                return render(request, 'weatherdress/registration.html', context)
+                context["error"] = error
+                context["form"] = form
+                return render(request, "weatherdress/registration.html", context)
             except User.DoesNotExist:
-
-                username    = form.cleaned_data['username']
-                first_name  = form.cleaned_data['first_name']
-                last_name   = form.cleaned_data['last_name']
-                password    = form.cleaned_data['password']
-                user        = User(
-                    username    = username,
-                    email = email,
-                    first_name  = first_name,
-                    last_name   = last_name,
+                username = form.cleaned_data["username"]
+                first_name = form.cleaned_data["first_name"]
+                last_name = form.cleaned_data["last_name"]
+                password = form.cleaned_data["password"]
+                user = User(
+                    username=username,
+                    email=email,
+                    first_name=first_name,
+                    last_name=last_name,
                 )
                 user.set_password(password)
                 user.save()
                 login(request, user)
-                return redirect('home')
+                return redirect("home")
 
-        
         else:
-            return render(request, 'weatherdress/registration.html', {'form': form})
+            return render(request, "weatherdress/registration.html", {"form": form})
 
     form = UserRegistrationForm()
-    context['form'] = form
+    context["form"] = form
 
-    return render(request, 'weatherdress/registration.html', context)
-
-
+    return render(request, "weatherdress/registration.html", context)
 
 
 def signout_page(request):
@@ -71,42 +65,69 @@ def signout_page(request):
     # dictionary = dict()
     # dictionary['displayname'] = listed[0]
     logout(request)
-    return render(request, 'bros/signout.html')
+    return render(request, "bros/signout.html")
+
 
 def signin_page(request):
     if request.method == "POST":
-        username  = request.POST['username']
-        password  = request.POST['password']
-        
-        #set requirements (parameters) for authentication
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        # set requirements (parameters) for authentication
         user = authenticate(username=username, password=password)
 
-       #if authenticated go home
+        # if authenticated go home
         if user:
             login(request, user)
-            return redirect('home')
-    
+            return redirect("home")
+
     form = UserLoginForm()
     context = {
         "form": form,
     }
-    return render(request, 'weatherdress/signin.html', context)
+    return render(request, "weatherdress/signin.html", context)
 
 
 def home(request):
-    API_KEY = open('apikey.txt', 'r').read()
-    ip_request = requests.get('https://get.geojs.io/v1/ip.json')
-    my_ip = ip_request.json()['ip']
-    geo_request = requests.get(f'https://get.geojs.io/v1/ip/geo/{my_ip}.json')
+    API_KEY = open("apikey.txt", "r").read()
+    ip_request = requests.get("https://get.geojs.io/v1/ip.json")
+    my_ip = ip_request.json()["ip"]
+    geo_request = requests.get(f"https://get.geojs.io/v1/ip/geo/{my_ip}.json")
     geo_data = geo_request.json()
     latitude = geo_data["latitude"]
     longitude = geo_data["longitude"]
 
-    request_url  = "https://api.openweathermap.org/data/3.0/onecall?lat={latitude}&lon={longitude}&appid={API_KEY}"
+    request_url = "https://api.openweathermap.org/data/3.0/onecall?lat={latitude}&lon={longitude}&appid={API_KEY}"
     response = requests.get(request_url).json()
 
-  
-        
+
+def recommend_activity(mood):
+    # Define a list of activities and their corresponding moods
+    activities = ["outdoor sports", "dancing", "meditation", "yoga", "reading"]
+    moods = ["energetic", "happy", "relaxed", "calm", "calm"]
+
+    # Convert the list of activities and moods into a matrix representation
+    X = np.array([activities, moods])
+    y = np.array(moods)
+
+    # Train a Naive Bayes classifier on the matrix representation
+    clf = MultinomialNB()
+    clf.fit(X, y)
+
+    # Use the trained classifier to predict the recommended activity based on the user's mood
+    mood_vector = np.array([mood])
+    return clf.predict(mood_vector)
 
 
+def recommend_activity_view(request):
+    if request.method == "POST":
+        mood = request.POST.get("mood")
+        activity = recommend_activity(mood)
+        return render(request, "recommend_activity.html", {"activity": activity})
+    else:
+        return render(request, "input_mood.html")
 
+
+def activity_form_view(request):
+    form = ActivityForm()
+    return render(request, "activity_form.html", {"form": form})
